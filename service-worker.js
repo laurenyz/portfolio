7e5b4a4bc2e8595796438dc244a9f2d63332511b
laurenyz <laurenkyu@gmail.com>
2020-05-13 16:49:21 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.68eb31e4ca037b7d2fec809fba3d439f.js"
+  "/portfolio/precache-manifest.f4390b57e593f99690f6a3bae8c799b6.js"
 );
 
 self.addEventListener('message', (event) => {
