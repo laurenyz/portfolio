15f249a7902bbeb963cd08f4ba078a95e5660016
laurenyz <laurenkyu@gmail.com>
2020-05-28 21:53:20 -0400

    removed unnecessary import

--- src/containers/ContactContainer.jsx
+++ src/containers/ContactContainer.jsx
@@ -3,7 +3,6 @@ import Avatar from '@material-ui/core/Avatar'
 import IconButton from '@material-ui/core/IconButton'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles(() => ({
     avatar: {
